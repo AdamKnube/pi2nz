83ed0430c79a8355b8da946cb0f8f37cfe92526e
AdamKnube <AdamKnube@5csecurity.com>
2016-01-20 22:56:12 -0500

    modified:   pi2nz.py (pretty)

--- pi2nz.py
+++ pi2nz.py
@@ -1,54 +1,61 @@
 #!/usr/bin/env python3
 #
 
+#==============================================================================#
+_debug_level_ = 3                                   # higher = spammier
+_bind_address_ = ('', 8080)                         # bind address/port
+_music_folder_ = '/mnt/store/Media/Audio/Music'     # mp3 music folder
+#==============================================================================#
+
 import os
 import threading
 import http.server
 
-_debug_level = 5
-_bind_address = ('', 8080)
-_music_folder = '/mnt/store/Media/Audio/Music'
+_DINFO_ = 1
+_DSPAM_ = 2
+_DTEST_ = 3
 
 # Debug printer
-def dprint(data='', level=0):
-    if (level > _debug_level): return
+def dprint(data='', dlevel=0):
+    if (dlevel > _debug_level_): return
     if (data == ''): return
-    print('[' + str(level) + ']  ' + data)
+    print('[' + str(dlevel) + ']  ' + data)
 
 # Music player
 class tunez_machine(threading.Thread):
     def __init__(self):
-        dprint("Music thread initializing", 1)
+        dprint("Music thread initializing", _DSPAM_)
+    
     def run(self):
-        dprint("Music thread running", 1)
+        dprint("Music thread running", _DINFO_)
 
 # Webserver backend
 class serv_backend(http.server.BaseHTTPRequestHandler):
     def do_HEAD(self):
-        dprint(self.requestline, 1)
+        dprint(self.requestline, _DINFO_)
         
     def do_GET(self):
-        dprint(self.requestline, 1)
+        dprint(self.requestline, _DINFO_)
         
     def go_POST(self):
-        dprint(self.requestline, 1)        
+        dprint(self.requestline, _DINFO_)        
 
 # Make an array of mp3 files    
 def getMusic(where=''):
     templist = []
-    dprint('Testing: ' + _music_folder, 1)
+    dprint('Testing: ' + _music_folder, _DINFO_)
     for root, folders, files in os.walk(where):
         for thisfile in files:
             absolute = os.path.join(root, thisfile)    
-            if ( thisfile[-3:].lower() == 'mp3'):
+            if ( thisfile[-4:].lower() == '.mp3'):
                 templist.append(absolute)
-                dprint('Added: ' + thisfile, 2)
-            else: dprint('Ignored: ' + thisfile + ' (' + thisfile[-3:].lower() + ')', 2)
+                dprint('Added: ' + thisfile, _DSPAM_)
+            else: dprint('Ignored: ' + thisfile + ' (' + thisfile[-4:].lower() + ')', _DSPAM_)
     return templist
 
 def runmain():
-    thelist = getMusic(_music_folder)
-    theserver = http.server.HTTPServer(_bind_address, serv_backend)
+    thelist = getMusic(_music_folder_)
+    theserver = http.server.HTTPServer(_bind_address_, serv_backend)
     theserver.serve_forever()    
     return 0
 
